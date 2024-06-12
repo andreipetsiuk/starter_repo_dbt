5c743567f165e8ecdc073c965ecf5ad8a0b90ca9
andreipetsiuk <andreipetsiuk@gmail.com>
2024-06-12 15:31:20 +0200

    changed a day

--- models/prep/prep_forecast_day.sql
+++ models/prep/prep_forecast_day.sql
@@ -7,7 +7,7 @@ add_features AS (
         ,date_part('day', date) AS day_of_month -- day of month as a number
         ,to_char(date, 'month') AS month_of_year -- month name as a text
         ,date_part('year', date) AS year -- year as a number
-        ,to_char(date, 'weekday') AS day_of_week -- weekday name as text
+        ,to_char(date, 'day') AS day_of_week -- weekday name as text
         ,date_part('week', date) AS week_of_year -- calender week number as number
         ,to_char(date, 'iyyy-iw') AS year_and_week -- year-calenderweek as text like '2024-43'
 
