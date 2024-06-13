4874257b6e0d2a5b0e46711b13ed1b1f26fe226d
andreipetsiuk <andreipetsiuk@gmail.com>
2024-06-13 15:11:27 +0200

    modified

--- models/mart/mart_forecast_hour.sql
+++ models/mart/mart_forecast_hour.sql
@@ -2,5 +2,6 @@ select
     staging_location.*,
     prep_forecast_hour.*,
     concat('&nbsp;&nbsp;&nbsp;&nbsp;![weather_icon](', prep_forecast_hour.condition_icon,'?width=35)') as condition_icon_md
-from {{ref'prep_forecast_hour'}}
-left join {{ref'staging_location'}} using(city)
+from {{ref('staging_location')}} 
+right join {{ref('prep_forecast_day')}} 
+	on staging_location.city = prep_forecast_day.city
