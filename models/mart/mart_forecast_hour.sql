3cd1dab113503b00236926e260a845f2e2bc0479
andreipetsiuk <andreipetsiuk@gmail.com>
2024-06-13 15:07:14 +0200

    changed the join to left join

--- models/mart/mart_forecast_hour.sql
+++ models/mart/mart_forecast_hour.sql
@@ -3,4 +3,4 @@ select
     prep_forecast_hour.*,
     concat('&nbsp;&nbsp;&nbsp;&nbsp;![weather_icon](', prep_forecast_hour.condition_icon,'?width=35)') as condition_icon_md
 from {{ref'prep_forecast_hour'}}
-right join {{ref'staging_location'}} using(city)
+left join {{ref'staging_location'}} using(city)
