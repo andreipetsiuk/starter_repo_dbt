929012347b425d39198c2eaaf7afb53e9a35c6f5
andreipetsiuk <andreipetsiuk@gmail.com>
2024-06-13 15:13:16 +0200

    changed

--- models/mart/mart_forecast_hour.sql
+++ models/mart/mart_forecast_hour.sql
@@ -3,5 +3,5 @@ select
     prep_forecast_hour.*,
     concat('&nbsp;&nbsp;&nbsp;&nbsp;![weather_icon](', prep_forecast_hour.condition_icon,'?width=35)') as condition_icon_md
 from {{ref('staging_location')}} 
-right join {{ref('prep_forecast_day')}} 
-	on staging_location.city = prep_forecast_day.city
+right join {{ref('prep_forecast_hour')}} 
+	on staging_location.city = prep_forecast_hour.city
