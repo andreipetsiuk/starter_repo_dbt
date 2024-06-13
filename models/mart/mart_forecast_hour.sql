1d201876c7543ff6a7f5d51d269402b30bf67bb1
andreipetsiuk <andreipetsiuk@gmail.com>
2024-06-13 15:00:49 +0200

    created mart_forecast_hour

---
+++ models/mart/mart_forecast_hour.sql
@@ -0,0 +1,6 @@
+select 
+    staging_location.*,
+    prep_forecast_day.*,
+    concat('&nbsp;&nbsp;&nbsp;&nbsp;![weather_icon](', prep_forecast_hour.condition_icon,'?width=35)') as condition_icon_md
+from {{ref'prep_forecast_hour'}}
+right join {{ref'staging_location'}}
